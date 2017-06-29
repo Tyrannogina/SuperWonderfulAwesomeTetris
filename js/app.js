ec41022aa7776c553163d4ea32e29dc2419e6756
tyrannogina <tyrannogina@gmail.com>
2017-06-29 20:33:32 +0200

    FRIGGIN' STYLES BABE!

--- js/app.js
+++ js/app.js
@@ -5,11 +5,14 @@ $(document).ready(function() {
   var cols = 10;
   game = new Game(rows, cols);
   game.drawBoard();
+  $('.instructions ul').hide();
+  $('.next-tetrominos .next').hide();
   $('#play-button').on('click', function() {
     game.assignControlsToKeys();
     game.start();
     $(this).prop('disabled', true);
     $('#reset-button').prop('disabled', false);
+    $('.next-tetrominos .next').show();
   });
   $('#pause-button').on('click', function(e) {
     e.preventDefault();
@@ -18,5 +21,10 @@ $(document).ready(function() {
   $('#reset-button').on('click', function(e) {
     e.preventDefault();
     game.reset(rows, cols);
+    $('.next-tetrominos .next').show();
+
+  });
+  $('.instructions').on('click', function() {
+    $('.instructions ul').toggle();
   });
 });
