74db8a531fca7277636e99c1222fbb70be3b6951
tyrannogina <tyrannogina@gmail.com>
2017-06-29 16:20:09 +0200

    Improve reset, fix levels and fix bonus points

--- js/app.js
+++ js/app.js
@@ -6,14 +6,17 @@ $(document).ready(function() {
   game = new Game(rows, cols);
   game.drawBoard();
   $('#play-button').on('click', function() {
+    game.assignControlsToKeys();
     game.start();
     $(this).prop('disabled', true);
+    $('#reset-button').prop('disabled', false);
   });
   $('#pause-button').on('click', function(e) {
     e.preventDefault();
     game.togglePause();
   });
   $('#reset-button').on('click', function(e) {
+    e.preventDefault();
     game.reset(rows, cols);
     game.start();
   });
--- js/game.js
+++ js/game.js
@@ -10,7 +10,188 @@ function Game(rows, columns) {
   this.followingTetrominos = [];
 }
 
-Game.prototype._assignControlsToKeys = function() {
+Game.prototype.start = function () {
+  this._generateTetrominos();
+  this.intervalID = setInterval(function () {
+    if (!this.isPaused) {
+      this.actualTetromino.moveTetromino('down');
+      this._drawTetromino();
+      if (this.actualTetromino.fix) {
+        this._fixToBottom();
+      }
+      if (this.actualTetromino.gameLost) {
+        this._gameOver();
+      }
+      this._deleteLines();
+    }
+  }.bind(this), 500);
+};
+
+Game.prototype.togglePause = function () {
+  if (this.isPaused) {
+    this.isPaused = false;
+  } else {
+    this.isPaused = true;
+  }
+};
+
+Game.prototype._generateRandomTetromino = function () {
+  return availableTetrominos[Math.floor(Math.random() * availableTetrominos.length)];
+};
+
+Game.prototype._generateTetrominos = function () {
+  for (var i = 0; i < 4; i++) {
+    var tetrominoConstructor = this._generateRandomTetromino();
+    if (i === 3) {
+      this.actualTetromino = new tetrominoConstructor();
+      this.actualTetromino.board = this.board;
+    } else {
+      this.followingTetrominos[i] = new tetrominoConstructor();
+    }
+  }
+  this._drawNextTetrominos();
+};
+
+Game.prototype._updateTetromino = function () {
+  this.actualTetromino = this.followingTetrominos[0];
+  this.actualTetromino.board = this.board;
+  var length = this.followingTetrominos.length;
+  for (var i = 0; i < length; i++) {
+    if (i === length - 1) {
+      this.followingTetrominos[i] = new (this._generateRandomTetromino())();
+    } else {
+      this.followingTetrominos[i] = this.followingTetrominos[i + 1];
+    }
+  }
+  this._drawNextTetrominos();
+};
+
+Game.prototype._thereAreFullLines = function () {
+  var fullLines = this.board.reduce(function (fullLinesAcc, line, lineIndex) {
+    var lineNotFull = line.some(function(element) {return element === false;});
+    if (!lineNotFull) {
+      fullLinesAcc.push(lineIndex);
+    }
+    return fullLinesAcc;
+  }, []);
+  if (fullLines.length > 0) {
+    return fullLines;
+  }
+  return false;
+};
+
+Game.prototype._deleteLines = function () {
+  var linesToDelete = this._thereAreFullLines();
+  if (linesToDelete !== false) {
+    //Add 100 points per line + bonus of number of lines (20 per 2 lines, 30 per 3...)
+    //Add 160 bonus points for every 4 lines so it is 600 points
+    var lineCount = linesToDelete.length;
+    var gotScore = (1 + (lineCount / 10)) * 100 + ( lineCount === 4? 160 : 0);
+    this._addToScore(gotScore);
+    linesToDelete.forEach(function (lineIndex) {
+      this.board.splice(lineIndex, 1);
+      this.board.unshift([]);
+      for (var j = 0; j < this.columns; j++) {
+        this.board[0].push(false);
+      }
+    }.bind(this));
+    this._refreshBoard();
+  }
+};
+
+Game.prototype._gameOver = function () {
+  clearInterval(this.intervalID);
+  alert("GAME OVER");
+  return false;
+};
+
+Game.prototype.reset = function (rows, columns) {
+  if (this.intervalID) {
+    clearInterval(this.intervalID);
+    this.intervalID = 0;
+  }
+
+  this.rows = rows;
+  this.columns = columns;
+  this.board = this._generateBoard();
+
+  this.isPaused = false;
+  this.score = 0;
+  this.level = 1;
+  this.actualTetromino = undefined;
+  this.followingTetrominos = [];
+  this._removeKeyAssignments();
+//  $('.actual').removeClass('actual');
+  this._refreshBoard();
+  return false;
+};
+
+Game.prototype._removeKeyAssignments = function () {
+  $('body').on('keydown', function(e) {
+    switch (e.keyCode) {
+      case 38: // arrow up
+        if (this.actualTetromino.name !== 'B') {
+          this.actualTetromino.rotateTetromino("clockwise");
+        }
+        break;
+      case 40: // arrow down
+        this.actualTetromino.moveTetromino("down");
+        this._addToScore(1);
+        break;
+      case 37: // arrow left
+        this.actualTetromino.moveTetromino("left");
+        break;
+      case 39: // arrow right
+        this.actualTetromino.moveTetromino("right");
+        break;
+      case 67: // c key
+        if (this.actualTetromino.name !== 'B') {
+          this.actualTetromino.rotateTetromino("counterclockwise");
+        }
+        break;
+      case 80: // p pause
+        this.togglePause();
+        break;
+    }
+    this._drawTetromino();
+  }.bind(this));
+};
+Game.prototype._fixToBottom = function () {
+  var rowStart = this.actualTetromino.offset.y < 0 ? Math.abs(this.actualTetromino.offset.y) : 0;
+  var length = this.actualTetromino.body.length;
+  for (var tRow = rowStart; tRow < length; tRow++) {
+    var lineNotEmpty = this.actualTetromino.body[tRow].some(function(element) {return element !== false;});
+    if (lineNotEmpty) {
+      for (var tCol = 0; tCol < length; tCol++) {
+        if (this.actualTetromino.body[tRow][tCol] !== false) {
+          this.board[(this.actualTetromino.offset.y + tRow)][this.actualTetromino.offset.x + tCol] = this.actualTetromino.name;
+        }
+      }
+    }
+  }
+  $('.actual').removeClass('actual');
+  this._addToScore(5);
+  this._updateTetromino();
+};
+
+Game.prototype._addToScore = function (valueToAdd) {
+  this.score += valueToAdd;
+  var tempLevel = Math.floor(this.score / 5000);
+  if (tempLevel + 1 > this.level) {
+    this._levelUp(tempLevel);
+  }
+  this._refreshScoreboard();
+};
+
+Game.prototype._levelUp = function (newLevel) {
+  this.level = newLevel;
+  this._refreshLevel();
+};
+
+
+/*********************** INTERFACE ********************************/
+
+Game.prototype.assignControlsToKeys = function() {
   $('body').on('keydown', function(e) {
     switch (e.keyCode) {
       case 38: // arrow up
@@ -67,14 +248,15 @@ Game.prototype.drawBoard = function () {
 };
 
 Game.prototype._refreshBoard = function () {
-  $('.cell').attr('class', 'cell');
   this._drawTetromino();
   this.board.forEach(function (row, rowIndex) {
     row.forEach(function (col, colIndex) {
+      var selector = '[data-row=' + rowIndex + ']' +
+                     '[data-col=' + colIndex + ']';
       if (this.board[rowIndex][colIndex] !== false) {
-        var selector = '[data-row=' + rowIndex + ']' +
-                       '[data-col=' + colIndex + ']';
         $(selector).addClass('tetro' + this.board[rowIndex][colIndex]);
+      } else {
+        $(selector).attr('class', 'cell');
       }
     }.bind(this));
   }.bind(this));
@@ -83,6 +265,7 @@ Game.prototype._refreshBoard = function () {
 Game.prototype._clearActualTetromino = function () {
   $('.actual').removeClass('actual ' + 'tetro' + this.actualTetromino.name);
 };
+
 Game.prototype._drawTetromino = function () {
   var rowStart = this.actualTetromino.offset.y < 0 ? Math.abs(this.actualTetromino.offset.y) : 0;
   var length = this.actualTetromino.body.length;
@@ -114,149 +297,10 @@ Game.prototype._drawNextTetrominos = function () {
   });
 };
 
-Game.prototype.start = function () {
-  this._assignControlsToKeys();
-  this._generateRandomTetrominos();
-  this.intervalID = setInterval(function () {
-    if (!this.isPaused) {
-      this.actualTetromino.moveTetromino('down');
-      this._drawTetromino();
-      if (this.actualTetromino.fix) {
-        this._fixToBottom();
-      }
-      if (this.actualTetromino.gameLost) {
-        this._gameOver();
-      }
-      this._deleteLines();
-    }
-  }.bind(this), 500);
-};
-
-Game.prototype.togglePause = function () {
-  if (this.isPaused) {
-    this.isPaused = false;
-  } else {
-    this.isPaused = true;
-  }
-};
-
-Game.prototype._generateRandomTetromino = function () {
-  return availableTetrominos[Math.floor(Math.random() * availableTetrominos.length)];
-};
-
-Game.prototype._generateRandomTetrominos = function () {
-  for (var i = 0; i < 4; i++) {
-    var tetrominoConstructor = this._generateRandomTetromino();
-    if (i === 3) {
-      this.actualTetromino = new tetrominoConstructor();
-      this.actualTetromino.board = this.board;
-    } else {
-      this.followingTetrominos[i] = new tetrominoConstructor();
-    }
-  }
-  this._drawNextTetrominos();
-};
-
-Game.prototype._updateTetromino = function () {
-  this.actualTetromino = this.followingTetrominos[0];
-  this.actualTetromino.board = this.board;
-  this.followingTetrominos[0] = this.followingTetrominos[1];
-  this.followingTetrominos[1] = this.followingTetrominos[2];
-  this.followingTetrominos[2] = new (this._generateRandomTetromino())();
-  this._drawNextTetrominos();
-};
-
-Game.prototype._thereAreFullLines = function () {
-  var fullLines = this.board.reduce(function (fullLinesAcc, line, lineIndex) {
-    var lineNotFull = line.some(function(element) {return element === false;});
-    if (!lineNotFull) {
-      fullLinesAcc.push(lineIndex);
-    }
-    return fullLinesAcc;
-  }, []);
-  if (fullLines.length > 0) {
-    return fullLines;
-  }
-  return false;
-};
-
-Game.prototype._deleteLines = function () {
-  var linesToDelete = this._thereAreFullLines();
-  if (linesToDelete !== false) {
-    if (linesToDelete.length > 5) {
-      //Add 100 bonus points for every 5 lines
-      this._addToScore(Math.floor(linesToDelete.length / 5)*100);
-    }
-    linesToDelete.forEach(function (lineIndex) {
-      this.board.splice(lineIndex, 1);
-      this.board.unshift([]);
-      for (var j = 0; j < this.columns; j++) {
-        this.board[0].push(false);
-      }
-      this._addToScore(100);
-    }.bind(this));
-    this._refreshBoard();
-  }
-};
-
-Game.prototype._gameOver = function () {
-  clearInterval(this.intervalID);
-  alert("GAME OVER");
-  return false;
-};
-
-Game.prototype.reset = function (rows, columns) {
-  clearInterval(this.intervalID);
-  this.rows = rows;
-  this.columns = columns;
-  this.board = this._generateBoard();
-  this.intervalID = undefined;
-  this.isPaused = false;
-  this.score = 0;
-  this.level = 1;
-  this.actualTetromino = undefined;
-  this.followingTetrominos = [];
-
-  this.start();
-  $('.actual').removeClass('actual');
-
-};
-Game.prototype._fixToBottom = function () {
-  var rowStart = this.actualTetromino.offset.y < 0 ? Math.abs(this.actualTetromino.offset.y) : 0;
-  var length = this.actualTetromino.body.length;
-  for (var tRow = rowStart; tRow < length; tRow++) {
-    var lineNotEmpty = this.actualTetromino.body[tRow].some(function(element) {return element !== false;});
-    if (lineNotEmpty) {
-      for (var tCol = 0; tCol < length; tCol++) {
-        if (this.actualTetromino.body[tRow][tCol] !== false) {
-          this.board[(this.actualTetromino.offset.y + tRow)][this.actualTetromino.offset.x + tCol] = this.actualTetromino.name;
-        }
-      }
-    }
-  }
-  $('.actual').removeClass('actual');
-  this._addToScore(5);
-  this._updateTetromino();
-};
-
-Game.prototype._addToScore = function (valueToAdd) {
-  this.score += valueToAdd;
-  var tempLevel = Math.floor(this.score / 5000);
-  if (tempLevel > this.level) {
-    this._levelUp(tempLevel);
-  }
-  this._updateScoreboard();
-};
-
-Game.prototype._updateScoreboard = function () {
-  $('#scoreboard').text(this.score);
-};
-
-Game.prototype._levelUp = function (newLevel) {
-  this.level = newLevel;
-  this._refreshLevel();
-};
-
 Game.prototype._refreshLevel = function () {
   $('#level').text(this.level);
+};
+
+Game.prototype._refreshScoreboard = function () {
+  $('#scoreboard').text(this.score);
 };
